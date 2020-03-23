9ab3ffd4fdcfc441d8bb4212b16f994d47d3a6ef
t_barun <pujanthapa88.63@gmail.com>
2020-03-23 22:24:44 +0545

    Settings change for deployment

--- covidData/settings.py
+++ covidData/settings.py
@@ -114,8 +114,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 FILE_UPLOAD_DIRECTORY_PERMISSIONS = 0o0777
 FILE_UPLOAD_PERMISSIONS = 0o0644
@@ -137,4 +146,3 @@ RQ_QUEUES = {
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
