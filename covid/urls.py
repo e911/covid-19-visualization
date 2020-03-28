92ec54373b8e3db00690df0e8f99a25d40eda235
t_barun <pujanthapa88.63@gmail.com>
2020-03-28 09:53:11 +0545

    Revise new ui url

--- covid/urls.py
+++ covid/urls.py
@@ -4,7 +4,7 @@ from covid.views import DailyDataList, DailyCountryList, NewDailyDataList
 
 urlpatterns = [
     path('', DailyDataList.as_view(), name="list"),
-    path('/new_ui_ongoing', NewDailyDataList.as_view(), name="new_list"),
+    path('new_ui_ongoing', NewDailyDataList.as_view(), name="new_list"),
     path('<int:country_id>/', DailyCountryList.as_view(), name="list_country_state"),
 ]
 
